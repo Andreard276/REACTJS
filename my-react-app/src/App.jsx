e71d8fda7e07d907aca799245a146a3ff2e484b2
andrea <240453@utxicotepec.edu.mx>
2026-01-22 14:20:37 -0600

    create and edic components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,78 @@
 function App(){
-  return( 
+  
+  
+  return ( 
     <div> 
     <h1>5A EVND</h1>
     <h2>Profesor:</h2>
     <h3>M.T.I Ricardo Luna Santos</h3>
-    <h3>Alumnos</h3>
-    <h4>Diana</h4>
-    <h4>Diego</h4>
-    <h4>Paola</h4>
-    <h4>Monica</h4>
+    <h4>Andrea</h4>
+
+    <UserComponent/>
+    <ProfileComponent/>
+    <FeedComponent/>
     
     </div>
    
   ) 
 }
+
+function UserComponent(){
+  const nombre = 'Andrea';
+  const apellidos = 'Rodriguez Morales';
+  const nombrecompleto = <h2>El nombre es: {nombre} y sus apellidos {apellidos}</h2>;
+  return <h1>User Component {nombrecompleto}</h1>;
+}
+
+function ProfileComponent(){
+  const users = [
+    {id: 1, name: 'Andrea', role: 'Web Developer'},
+    {id: 2, name: 'Diego', role: 'Web Designer'},
+    {id: 3, name: 'Paola', role: 'Team Leader'},]
+  return (
+    <>
+    <p>Lista de usuarios del sistema</p>
+    <ul>
+      {
+      users.map (function(user,index) {
+        return (
+          <li key={index}>{user.name} es un {user.role}</li>
+        )
+      })
+    }
+    </ul>
+    </>
+
+  );
+}
+
+function FeedComponent(){
+  const users=[
+    {id:1, name:'pala', role:'Materiales de construcción'},
+    {id:2, name:'martillo', role:'Herramientas de construcción'},
+    {id:3, name:'cemento', role:'Materiales de construcción'},
+    {id:4, name:'ladrillo', role:'Materiales de construcción'},
+    {id:5, name:'nivel', role:'Herramientas de construcción'},
+  ]
+   return (
+    <>
+    <p>Lista de materiales del sistema</p>
+    <ul>
+      {
+      users.map (function(user,index) {
+        return (
+          <li key={index}>{user.name} es un {user.role}</li>
+        )
+      })
+    }
+    </ul>
+    </>
+
+  );
+}
+
+ 
+  
+
 
 export default App
