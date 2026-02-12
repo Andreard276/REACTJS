661e0bc98abda700ed8dee5305c11d1dc73b9ac0
andrea <240453@utxicotepec.edu.mx>
2026-02-12 14:18:56 -0600

    se agrefo la direccion

--- my-react-app/src/Promociones.jsx
+++ my-react-app/src/Promociones.jsx
@@ -6,6 +6,7 @@ function Promociones() {
       <div className="promociones-content">
         <h2>Saruino Design RM</h2>
         <p>Saruino Design RM es una empresa que busca capturar el arte de lo cotidiano atraves de tradiciones, fotografia, videos, musica, tatuajes, hobbies y moda.</p>
+        <mapa/>
       </div>
     </section>
   );
